5f1fe2cf7d96bc0c025a20e16622789263e602e8
MinkolaUK <minkola@gmail.com>
2024-07-23 19:10:05 +0100

    Added new files

---
+++ const/contractAddresses.ts
@@ -0,0 +1,32 @@
+/** Replace the values below with the addresses of your smart contracts. */
+
+// 1. Set up the network your smart contracts are deployed to.
+// First, import the chain from the package, then set the NETWORK variable to the chain.
+import { MordorTestnet} from "@thirdweb-dev/chains";
+export const NETWORK = MordorTestnet;
+
+export const ETHERSCAN_URL = "https://etc-mordor.blockscout.com";
+
+// 2. The address of the marketplace V3 smart contract.
+// Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
+export const MARKETPLACE_ADDRESS = "0xcb1043277cEf7B7f1CC811CcE0321b9Ba643a3d7";
+export const NFT_ADDRESS = "0xD7BfC3360138C78F7c286C755964c45BB35cA6a0";
+export const REWARDSADDRESS = "0x9E30F684E03774D0380aa6c6A8151e4789b98a6d";
+export const stakingContractAddress = "0x9E30F684E03774D0380aa6c6A8151e4789b98a6d";
+export const tokenContractAddress = "0xcbb3DFb966C2A552E486276FA2813BA5618541D1";
+export const currency = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+
+//3. Descriptions
+export const title = "Mink NFT Collection";
+export const description = `"MINK NFT's" Are you  ready to join your digital revolution today.`;
+export const welcome = "Welcome to the MINK NFT Collection";
+export const maxSupply = 50;
+export const mintPrice = 0.2;
+export const projectNfts = "NFTs";
+export const maxPerWallet = 50;
+
+//4. Social Media
+export const twitterLink = "https://x.com/Mink11904498503";
+export const discordLink = "https://discord.com/invite/xDTuxBVK"
+export const telegramLink = "https://discord.com/invite/xDTuxBVK"
+
