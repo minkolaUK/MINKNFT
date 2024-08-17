08ab936912815c35ff95a246492ad0afc1354edf
MinkolaUK <minkola@gmail.com>
2024-08-17 15:34:20 +0100

    Dex Contract

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -69,7 +69,7 @@ contract ETCDex is Ownable, ReentrancyGuard {
         }
 
         _liquidityProviders[msg.sender] = _liquidityProviders[msg.sender].add(liquidity);
-        _stakingTimestamps[msg.sender] = block.timestamp;
+        _stakingTimestamps[msg.sender] = block.timestamp; // Update staking timestamp
         _updateReward(msg.sender);
 
         emit LiquidityAdded(msg.sender, tokenAmount, msg.value, liquidity);
